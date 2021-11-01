634d8efb04fda928e560b63fc906aa61c96ef140
specktro <specktro@nonull.mx>
2021-11-01 18:32:02 -0400

    chore: Create a simple function to load an image

--- Networking.playground/Contents.swift
+++ Networking.playground/Contents.swift
@@ -2,31 +2,10 @@ import UIKit
 import PlaygroundSupport
 import Foundation
 
-// MARK: MyViewController class
-final class MyViewController : UIViewController {
-  // MARK: - Properties
+class Network {
   private let imageURL: String = "https://goo.gl/wV9G4I"
-  private weak var imageView: UIImageView!
   
-  // MARK: - Lifecycle methods
-  override func loadView() {
-    let view: UIView = UIView()
-    view.backgroundColor = .white
-    
-    let imageView: UIImageView = UIImageView(frame: .zero)
-    imageView.translatesAutoresizingMaskIntoConstraints = false
-    view.addSubview(imageView)
-    self.imageView = imageView
-    
-    self.view = view
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-    
+  func getImage(_ completion: @escaping () -> ()) {
     // Try to get a valid url from image url string
     guard let url: URL = URL(string: imageURL) else {
       preconditionFailure("Thre was a problem getting the url from string")
@@ -52,12 +31,38 @@ final class MyViewController : UIViewController {
       let image: UIImage? = UIImage(data: data)
       
       // If you are going to update the user interface it must be done into the main thread
-      DispatchQueue.main.sync { [weak self] in
-        self?.imageView.image = image
-      }
+//      DispatchQueue.main.sync { [weak self] in
+//        self?.imageView.image = image
+//      }
     }
     
     dataTask.resume()
+  }
+}
+
+// MARK: MyViewController class
+final class MyViewController : UIViewController {
+  // MARK: - Properties
+  private weak var imageView: UIImageView!
+  
+  // MARK: - Lifecycle methods
+  override func loadView() {
+    let view: UIView = UIView()
+    view.backgroundColor = .white
+    
+    let imageView: UIImageView = UIImageView(frame: .zero)
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(imageView)
+    self.imageView = imageView
+    
+    self.view = view
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
   }
 }
 
